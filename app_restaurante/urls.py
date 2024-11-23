b523bd2ce8108e458e3132111680e8d8ee426679
jdacostac <juanzho1301@outlook.com>
2024-11-23 03:58:06 -0500

    cambios en comensal

--- app_restaurante/urls.py
+++ app_restaurante/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('menu/', views.menu_vista, name='menu'),
-    path('comensal/', views.comensal, name='comensal')
+    path('registro_comensal/', views.registro_comensal, name='registro_comensal'),
+    path('listar_comensales/', views.listar_comensales, name='listar_comensales'),
+    path('listar_comensales/registro_comensal/', views.registro_comensal, name='listar_comensales_registro'),
+    path('registro_comensal/listar_comensales/', views.listar_comensales, name='registro_listar_comensales'),
+    path('registro_comensal/listar_comensales/registro_comensal/', views.registro_comensal, name='registro_listar_y_registro_comensal'),
+    path('eliminar_comensal/<int:cedula>/', views.eliminar_comensal, name='eliminar_comensal'),
 ]
--- app_restaurante/views.py
+++ app_restaurante/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ComensalForm
+from .models import comensal
 # Create your views here.
 
 def menu_vista(request):
     opciones = ['Comensal', 'Reserva', 'Menu', 'Plato', 'Personal']
     return render(request, 'menu.html', {'opciones': opciones})
 
-def comensal(request):
+def registro_comensal(request):
     if request.method == 'POST':
         form = ComensalForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('comensal')
+            return redirect('registro_comensal')
     else:
         form = ComensalForm()
 
-    return render(request, 'comensal.html', {'form': form})
+    return render(request, 'registro_comensal.html', {'form': form})
+
+def listar_comensales(request):
+    comensales = comensal.objects.all() 
+    return render(request, 'listar_comensales.html', {'comensales': comensales})
+
+def eliminar_comensal(request, cedula):
+    comensal_a_eliminar = get_object_or_404(comensal, cedula=cedula)
+    comensal_a_eliminar.delete()
+    return redirect('listar_comensales')
